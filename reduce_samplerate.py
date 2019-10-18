9953efa446b1622d52cf175ba311639cc44e99a2
Edvard Lindelöf <edvardlindelof@gmail.com>
2019-10-18 10:30:47 +0200

    load_reduced_wav function in reduce_samplerate.py with main scopes that checks the quality reduction

--- reduce_samplerate.py
+++ reduce_samplerate.py
@@ -1,48 +1,33 @@
-import simpleaudio as sa
 import samplerate
-import librosa
 from scipy.io import wavfile
 import numpy as np
 
 
 SECONDS = 3
-DOWN_SAMPLING_FACTOR = 2
+DOWN_SAMPLING_FACTOR = 5
 
-wav_path ='/home/edvard/SharedWithVirtualBox/genres/blues/blues.00000.wav'
-rate, wav = wavfile.read(wav_path)
-print(rate)
-wav, rate = librosa.load(wav_path)
-#wav = wav[:SECONDS*rate]
-print(wav)
+INT16_MAX = 2.0 ** (16 - 1) + 1
 
-wav = librosa.resample(wav, rate, rate / DOWN_SAMPLING_FACTOR)
-wav = (wav * (2.0 ** (16-1) + 1)).astype(np.int16)
-print(wav.dtype)
-
-rate = int(rate / DOWN_SAMPLING_FACTOR)
-play_obj = sa.play_buffer(wav, 1, 2, rate)
-play_obj.wait_done()
+def load_reduced_wav(path):
+    rate, wav = wavfile.read(path)  # loads as int16
+    wav = (wav[:SECONDS*rate] / INT16_MAX).astype(np.float)  # take first SECONDS and convert to [-1.0, 1.0]
+    wav_downsampled = samplerate.resample(wav, 1 / DOWN_SAMPLING_FACTOR)
+    return wav_downsampled
 
 
-##wav_downsampled = samplerate.resample(wav, 1 / DOWN_SAMPLING_FACTOR)
-#wav_upsampled = samplerate.resample(wav_downsampled, DOWN_SAMPLING_FACTOR)
-##wav_upsampled = (wav_upsampled * (2.0 ** (16-1) + 1)).astype(np.int)
-#print(wav)
-#print(wav_upsampled)
-#print(wav_upsampled.shape)
-#play_obj = sa.play_buffer(wav_upsampled, 1, 2, rate)
-##play_obj = sa.play_buffer(wav, 1, 2, rate)
-#play_obj.wait_done()
-#
-#print(wav[:10])
-#wav = wav[:SECONDS*rate]
-#wav = wav[::DOWN_SAMPLING_FACTOR]
-#wav = np.ascontiguousarray(wav)  # because of low level C stuff
-#rate = int(rate / DOWN_SAMPLING_FACTOR)
-#print(wav[:10])
-#
-#print(wav.shape)
-#play_obj = sa.play_buffer(wav, 1, 2, rate)
-#print(play_obj.is_playing())
-#play_obj.wait_done()
+if __name__ == '__main__':
+    import simpleaudio as sa
+
+    wav_path = '/home/edvard/SharedWithVirtualBox/genres/blues/blues.00000.wav'
+
+    wav_downsampled = load_reduced_wav(wav_path)
+    wav = samplerate.resample(wav_downsampled, DOWN_SAMPLING_FACTOR)
+    wav = (wav * INT16_MAX).astype(np.int16)
+
+    print('original size: {}'.format(wav.shape))
+    print('downsampled size: {}'.format(wav_downsampled.shape))
+
+    play_obj = sa.play_buffer(wav, 1, 2, 22050)  # rate returned by wavfile.read
+    play_obj.wait_done()
+
 
