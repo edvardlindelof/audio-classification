f1c04b6fc9446e98b60be128b6ce1e941a0f2816
Edvard Lindelöf <edvardlindelof@gmail.com>
2019-10-23 14:46:17 +0200

    added MaxPool, BatchNorm and Dropout, and decreased batch size

--- train_cnn.py
+++ train_cnn.py
@@ -11,7 +11,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--path', type=str, default='/home/edvard/SharedWithVirtualBox/genres')
 parser.add_argument('--epochs', type=int, default=1000)
-parser.add_argument('--batch-size', type=int, default=100)
+parser.add_argument('--batch-size', type=int, default=50)
 parser.add_argument('--mfcc', action='store_true', help='Transform wave to MFCCs before feeding')
 
 parser.add_argument('--validate-every', type=int, default=10)
@@ -23,18 +23,21 @@ class CNN1D(nn.Module):
     def __init__(self, in_channels=1):
         super(CNN1D, self).__init__()
         # TODO sort out the layer scaling in some way that works neatly when mfccs are fed
-        self.block1 = nn.Sequential(nn.Conv1d(in_channels, 5, int(5000 / (in_channels ** 2)), int(100 / in_channels)), nn.ReLU())
-        self.block2 = nn.Sequential(nn.Conv1d(5, 25, 30, int(10 / (in_channels ** 0.5))), nn.ReLU())
-        self.block3 = nn.Sequential(nn.Conv1d(25, 125, 10, 10), nn.ReLU())
-        #self.block4 = nn.Sequential(nn.Conv1d(125, 625, 10, 10), nn.ReLU())
+        self.block1 = nn.Sequential(nn.Conv1d(in_channels, 25, 5), nn.MaxPool1d(4), nn.ReLU(), nn.BatchNorm1d(25))
+        self.block2 = nn.Sequential(nn.Conv1d(25, 50, 5), nn.MaxPool1d(4), nn.ReLU(), nn.BatchNorm1d(50))
+        self.block3 = nn.Sequential(nn.Conv1d(50, 100, 5), nn.MaxPool1d(4), nn.ReLU(), nn.BatchNorm1d(100))
+        self.block4 = nn.Sequential(nn.Conv1d(100, 200, 4), nn.ReLU(), nn.Dropout(0.3))
+        self.block5 = nn.Sequential(nn.Conv1d(200, 10, 1), nn.ReLU())
 
     def forward(self, input):
         x0 = input.view(input.size(0), -1, input.size(-1))
         x1 = self.block1(x0.float())  # TODO remove need to call float here
         x2 = self.block2(x1)
         x3 = self.block3(x2)
-        embedding = x3.squeeze(-1)
-        return embedding
+        x4 = self.block4(x3)
+        x5 = self.block5(x4)
+        pred_logits = x5.view(-1, 10)
+        return pred_logits
 
 
 class ClassificationCNN1D(nn.Module):
@@ -72,13 +75,14 @@ if __name__ == '__main__':
     dataset_val = audio_data.AudioDataset(args.path, n_per_class=samples_per_class_val, n_skip=80, mfcc=args.mfcc)
     dataloader_val = data.DataLoader(dataset_val, batch_size=100)
 
-    net = ClassificationCNN1D(in_channels)
+    net = CNN1D(in_channels)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters())
 
     for epoch in range(args.epochs):
         song, label = next(iter(dataloader_train))
         optimizer.zero_grad()
+        net.train()  # bc of batchnorm
         output = net(song)
         loss = criterion(output, label)
         loss.backward()
@@ -87,6 +91,7 @@ if __name__ == '__main__':
         if epoch % args.validate_every == 0:
             with torch.no_grad():
                 song, label = next(iter(dataloader_val))
+                net.eval()  # bc of batchnorm
                 output = net(song)
                 label_pred = np.argmax(output, axis=1)
                 accuracy = np.mean(np.array(label_pred) == np.array(label))
